350b4eec9ebc5ca4f3a14b326bc2e617ad24b819
BrownKnight <amanrk@hotmail.co.uk>
2021-01-16 19:40:55 +0000

    Improve user CRUD tests

--- OpenWallet-api/src/test/java/com/openwallet/api/controllers/UserControllerIT.java
+++ OpenWallet-api/src/test/java/com/openwallet/api/controllers/UserControllerIT.java
@@ -58,17 +58,20 @@ class UserControllerIT extends BaseIntegrationTest {
         As.Admin()
                 .putRequest("/api/v1/users", user)
                 .andDo(print())
+                .andDo((data) -> user.setId(objectMapper.readValue(data.getResponse()
+                        .getContentAsString(), User.class)
+                        .getId()))
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString(user.getFirstName())));
 
         As.Admin()
-                .putRequest("/api/v1/users", Map.of("id", 3, "lastName", "Changed"))
+                .putRequest("/api/v1/users", Map.of("id", user.getId(), "lastName", "Changed"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString("Changed")));
 
         As.Admin()
-                .getRequest("/api/v1/users")
+                .getRequest(String.format("/api/v1/users/%s", user.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string(containsString(user.getFirstName())));
